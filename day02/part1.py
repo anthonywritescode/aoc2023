3851a64c408871c21b3cfa03c1f087cad7d3a883
Anthony Sottile <asottile@umich.edu>
2023-12-02 00:08:13 -0500

    day 2!

---
+++ day02/part1.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    m = {'red': 12, 'green': 13, 'blue': 14}
+    lines = s.splitlines()
+    total = 0
+    for line in lines:
+        p1, rest = line.split(': ')
+        game_id = int(p1.split()[1])
+
+        for draw in rest.split('; '):
+            parts = [part.split() for part in draw.split(', ')]
+            if not all(m[color] >= int(n) for n, color in parts):
+                break
+        else:
+            total += game_id
+
+    return total
+
+
+INPUT_S = '''\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+'''
+EXPECTED = 8
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ day02/part2.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import argparse
+import math
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+    for line in lines:
+        _, rest = line.split(': ')
+        max_n = {'red': 0, 'green': 0, 'blue': 0}
+        for draw in rest.split('; '):
+            parts = [part.split() for part in draw.split(', ')]
+            for n, color in parts:
+                max_n[color] = max(max_n[color], int(n))
+
+        total += math.prod(max_n.values())
+
+    return total
+
+
+INPUT_S = '''\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+'''
+EXPECTED = 2286
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
